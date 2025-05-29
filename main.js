af3d6bc89363845586cbbed90a6b7c56511a8fb3
PedroSales <pedro.santos013@sptech.school>
2025-05-29 15:36:08 -0300

    Dados redirecionado para a VM em computadores diferentes

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: '127.0.0.1',
+            host: '10.18.33.33',
             user: 'aluno',
             password: 'Sptech#2024',
             database: 'DB_FitAlert',
